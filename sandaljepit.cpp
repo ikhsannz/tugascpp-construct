1807394192e016425779820f344727ec3233a2f0
Khairul Ikhsan Nurzeha <32305997+ikhsannz@users.noreply.github.com>
2018-01-20 01:50:53 +0800

    merapikan baris dan memberikan keterangan

--- sandaljepit.cpp
+++ sandaljepit.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 
+//class dan variabel
 class sandaljepit{
 	public:
 		void bahan (string dibuat);
@@ -10,25 +11,33 @@ class sandaljepit{
 		string warna;
 };
 int main(){
+//varian sandaljepit
 	sandaljepit nippon,swallow,ando;
-	
+
+//isi data nippon
 	nippon.warna 	= "putih biru ";
 	nippon.ukuran 	= 43 ;
+//isi data swallow
 	swallow.warna 	= "hijau merah ";
 	swallow.ukuran 	= 42 ;
+//isi data ando
 	ando.warna 		= "kuning hitam ";
 	ando.ukuran		= 45 ;
-	
+
+//cout nippon	
 	cout << (nippon.warna);
 	cout << (nippon.ukuran)<<" ";
 	nippon.bahan("karet ");
+//cout swallow
 	cout << (swallow.warna);
 	cout << (swallow.ukuran)<<" ";
 	swallow.bahan("plastik ");
 	cout << (ando.warna);
+//cout ando
 	cout << (ando.ukuran)<<" ";
 	ando.bahan("kulit ");
 }
+//output void
 void sandaljepit ::bahan(string dibuat){
 	cout<<"bahannya "<<dibuat<<endl;
 }
